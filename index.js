9718e1f4b8081fed51695b32f39c78658aaed16b
Amine Kebichi <amkebichi@gmail.com>
2022-09-12 02:34:40 -0400

    incorperated bg pattern

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 //lol idk
+
+function myfunc(){
+    var mydiv = document.createElement("div");
+    mydiv.innerHTML = `
+    <div class="bg-wrapper" id="fractal" style="float: left; z-index: 0;">
+        <div class="triangle-up"></div>
+        <div class="square"></div>
+        <div class="center-wrapper">
+            <div class="triangle-left"></div>
+            <div class="square"></div>
+            <div class="square"></div>
+            <div class="square"></div>
+            <div class="triangle-right"></div>
+        </div>
+        <div class="square"></div>
+        <div class="triangle-down"></div>
+    </div>
+    `;
+
+    document.getElementById('temp').appendChild(mydiv);
+}
+
+for(var i=0; i<9999; i++){
+    myfunc();
+}
